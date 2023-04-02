fd61d149acdb20d09600c6c6b18e98345b40d0be
Connor <connor5741213@gmail.com>
2023-04-02 17:14:29 -0400

    initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <React.Fragment>
+        <Navbar>
+        </Navbar>
+      </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,31 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "../Styles/main.css"
+
+function Navbar() {
+    const navRef = useRef();
+
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav")
+    }
+
+    return ( 
+        <header>
+            <h3>Logo</h3>
+            <nav ref={navRef}>
+                <a href="/">history</a>
+                <a href="/#">2222</a>
+                <a href="/#">3333</a>
+                <a href="/#">contact</a>
+                <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+                    <FaTimes />
+                </button>
+            </nav>
+            <button className="nav-btn" onClick={showNavbar}>
+                <FaBars />
+            </button>
+        </header>
+     );
+}
+
+export default Navbar;
