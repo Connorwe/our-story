a59fc808f3f4f4b3c341a96607f17c1e4cbc156d
Connor <connor5741213@gmail.com>
2023-05-25 06:03:50 -0400

    most of new kingdom finished

--- src/Components/History/Bronze-Age/bronzeAge.jsx
+++ src/Components/History/Bronze-Age/bronzeAge.jsx
@@ -7,6 +7,7 @@ import Minoan1 from "../assets/minoan-1.jpg"
 import Minoan2 from "../assets/minoan-2.jpg"
 import Mentuhotep from "../assets/Mentuhotep Nebhepetre.jpeg"
 import SesostrisIII from "../assets/Sesostris-III.jpg"
+import SecondIntermediate from "../assets/SecondIntermediate.png"
 function BronzeAge() {
 
     const [selectedSection, setSelectedSection] = useState("");
@@ -101,7 +102,7 @@ function BronzeAge() {
                 </div>
                 <div className="content-wrapper">
                     <div className="left-content">
-                        <h4 className="egypt-header-2">(Middle Kingdom 12th and 13th dynasties):</h4>
+                        <h4 className="egypt-header-2">Middle Kingdom 12th and 13th dynasties:</h4>
                         <p>
                             Amenemhet I was murdered while Sesostris was on a campaign in Libya. Sesostris kept the throne but had much internal unrest, he campaigned to Syria and
                             Palestine and in turn broke the traditional view that the Middle Kingdom didn't advance toward the Middle East. Not much is known of the reigns of Amenemhet II and
@@ -120,6 +121,101 @@ function BronzeAge() {
                         <img src={SesostrisIII} alt="Sesostris III" />
                     </div>
                 </div>
+                <div className="content-wrapper">
+                    <div className="left-content">
+                        <h4 className="egypt-header-2">Second Intermediate Period (1650-1550BCE):</h4>
+                        <p>
+                            This period of Egypt was very divided with the Hyksos in the North, the Egyptians in the center, and the Nubians in the south. The Hyksos people gained control of the
+                            north because, during the 13th dynasty, the Egyptian rulers moved the capital south to Thebes. During the 13th dynasty, immigration brought Semitic people to Avaris founded
+                            by King Amenemhat I. These people named Hyksos meaning (rulers of foreign lands) gained wealth and political status and influenced the country. The Nubians/ Kush to the
+                            South continued to trade with Egyptians and remained on cordial terms with the Egyptians. This period was not nearly as chaotic and lawless as the first intermediate period was.
+                            However Egyptian king Seqenenra Taa of the 17th dynasty led an expedition against the Hyksos because the Hyksos king Apepi insulted him which was received as a challenge. King
+                            Seqenenra Taa took an army to Avaris and attacked the Hyksos but were defeated with Seqenenra dying in battle (according to his mummy). After his death, his son Kamose
+                            ruled at Thebes and continued the war stating that he should not have to share his country with foreign powers. Kamose took an army on the Nile and released them on the city
+                            of Avaris and destroyed it. The Hyksos people still held Memphis so Kamose fought them for another 3 years and may have captured it from them. His successor was his brother
+                            Ahmose I who was the founder of the 18th Dynasty and the New Kingdom.
+                        </p>
+                    </div>
+                    <div className="right-content">
+                        <img src={SecondIntermediate} alt="Egypt Map" />
+                    </div>
+                </div>
+                <div className="content-wrapper">
+                    <div className="left-content">
+                        <h4 className="egypt-header-2">New Kingdom(1550-1069BCE):</h4>
+                        <p>
+                            Egypt was much more literate during this time making this period very well documented by historians.
+                            Ahmose I began to expand the borders of Egypt to ward off any other invasions. Pharaohs after him took the same approach eventually making Egypt into an Empire. This made
+                            Egypt very powerful and known well across the world and increased diplomatic relations with other Powers. Hatshepsut the first female Pharoh of Egypt was very powerful
+                            and ruled in peace with Nubia to the South unlike her father Thutmose I who killed the Nubian King before she became Pharoh. Hatshepsut had many building projects having
+                            only fewer than Ramesses the Great. Her name was removed from a lot of her projects by her son in order to sustain gender roles in Egyptian society. Her son Thutmose III
+                            created the Egyptian Empire which would be maintained by his successors. Thutmose went on many military campaigns gaining control of lands like Syria and Libya.
+                            Amenhotep II inherited this strong kingdom and made it better. He made peace treaties started building projects and traded with other lands. After him
+                            Thutmose IV kept his policies and is best known as the restorer of the Great Sphinx created during the Old Kingdom. His successor was Amenhotep III who ruled
+                            Egypt at one of its highest points politically and economically. More below.
+                        </p>
+                    </div>
+                    <div className="right-content">
+                        {/* <p>Sesostris III</p>
+                        <img src={SesostrisIII} alt="Sesostris III" /> */}
+                    </div>
+                </div>
+                <div className="content-wrapper">
+                    <div className="left-content">
+                        <h4 className="egypt-header-2">New Kingdom (Continued):</h4>
+                        <p>
+                            Amenhotep III used his wealth to make other countries do what he desired. During his reign, the Amun priests gained more power and wealth. He tried to stop
+                            their power by showing liking to the God Aten instead of Amun in an effort to discredit the Amun priests. This did not work however but did give more status to
+                            the God Aten. Amenhotep IV denounced monotheism (old religion) and moved the capital to Amarna then changed his name to Akhenaten. He made Aten the only true God
+                            and the Cult of Aten was the only religious group viewed as legitimate. Akhetaten's wife was Nefertiti known for her sculpture created by Thutmose. His son Tutankhaten
+                            succeeded the throne and changed his name to Tutankhamun, then he reverted everything that his father did back to the way it was. He moved the capital to Memphis,
+                            re-opened the temples, and brought back the old religion. After his death general Horemheb gained power then destroyed all traces of the Amarna Period of Kings before
+                            him and tried to restore Egypt to its ancient ways. He mostly succeeded in doing this but died with no heir to the throne so the vizier Paramesse became King naming himself
+                            Ramesses I starting the 19th dynasty.
+                        </p>
+                    </div>
+                    <div className="right-content">
+                        {/* <p>Sesostris III</p>
+                        <img src={SesostrisIII} alt="Sesostris III" /> */}
+                    </div>
+                </div>
+                <div className="content-wrapper">
+                    <div className="left-content">
+                        <h4 className="egypt-header-2">New Kingdom 19th Dynasty:</h4>
+                        <p>
+                            Ramesses I was an old man when he took the throne and made sure to appoint his son Seti I as his successor immediately. He continued to build on Horemheb's
+                            ideas while sending his son on military campaigns to take back the territory lost during Akhenaten's reign. When Ramesses died his son continued his work on
+                            the temple of Amun at Karnak and the restoration of the Amun religion. Ramesses II also known as Ramesses the Great is the son of Seti I and one of the most
+                            documented Pharaohs ever. He built monuments everywhere and spread his name throughout Egypt. He is shown as a great warrior who defeated the Hittites signing
+                            the world's first peace treaty (Kadesh Peace Treaty 1269BCE). He built a new city in Avaris named Per-Ramesses that he divided into 4 sections dedicating
+                            each to one God. Two Asiatic deities and two Egyptian. This may have been an attempt to integrate cultures with former enemies. The Cult of Amun in Thebes
+                            grew stronger because of the capital move and allowed the priests to gain power parallel with the pharaohs. Ramesses II died at 90-96 years old causing
+                            public alarm because he had been king for so long. His son Merenptah inherited the throne as his 13th son because his other sons died. Merenptah was much like
+                            his father defeating Sea Peoples and Libyans and becoming a warrior. He was succeeded by a usurper named Amenmesse who took power from Seti II the rightful heir.
+                            Seti II took power after Amenmesse and followed his father's ideals. Merenptah Siptah succeeded him at age 10 and died at age 16. He was succeeded by Setnakhte a usurper
+                            who founded the 20th dynasty.
+                        </p>
+                    </div>
+                    <div className="right-content">
+                        {/* <p>Sesostris III</p>
+                        <img src={SesostrisIII} alt="Sesostris III" /> */}
+                    </div>
+                </div>
+                <div className="content-wrapper">
+                    <div className="left-content">
+                        <h4 className="egypt-header-2">New Kingdom 20th Dynasty:</h4>
+                        <p>
+
+                        </p>
+                    </div>
+                    <div className="right-content">
+                        {/* <p>Sesostris III</p>
+                        <img src={SesostrisIII} alt="Sesostris III" /> */}
+                    </div>
+                </div>
+                {/* During the 18th and 19th Dynasties Egypt was at its most
+                            powerful with a stong centralized government with the Pharoh at the center. This started to come to an end during the 20th dynasty as priests of Amun started to
+                            gain status at Thebes and divided Egypt between Pharoh and priest. */}
             </div>
             <BronzeAgeFooter />
         </div>
